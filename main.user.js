b3196e7b7d15d5b4f25e381510aed196da6f042a
Grégory <prodtv13@gmail.com>
2015-07-04 00:03:31 +0200

    add user script

---
+++ main.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        CustomChat v2
+// @description Un script pour customiser la shoutbox de RG !
+// @include     http://realitygaming.fr/
+// @include     https://realitygaming.fr/
+// @include     http://www.realitygaming.fr/
+// @include     https://www.realitygaming.fr/
+// @include     http://realitygaming.fr/chatbox/
+// @include     https://realitygaming.fr/chatbox/
+// @include     http://www.realitygaming.fr/chatbox/
+// @include     https://www.realitygaming.fr/chatbox/
+// @version     2.0
+// @grant       none
+// ==/UserScript==
+
+// Script by Wayz. Tous droit résérvés !
+
+$(document).ready(function(){
+	$.getScript('https://cdn.rawgit.com/gregory-yet/ChatCustom/master/main.js');
+});
